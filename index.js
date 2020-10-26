e938e05a483b3f460b8987bdc0be503b901f5d95
Carlos <carloshenriquexvii@gmail.com>
2020-10-26 09:17:33 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const convert = require('./lib/convert');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/cotacao', (req, res) => {
+  const { cotacao, quantidade } = req.query;
+  if (cotacao === "" || quantidade === "") {
+    res.render('erro');
+  }
+  const conversao = convert.toMoney(convert.convert(cotacao, quantidade));
+  res.render('cotacao', {
+    cotacao: convert.toMoney(cotacao),
+    quantidade,
+    conversao
+  })
+})
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+app.listen(3000, err => {
+  if (err) {
+    console.log('Não foi possível iniciar.')
+  } else {
+    console.log('ConvertMyMoney online.')
+  }
+})
